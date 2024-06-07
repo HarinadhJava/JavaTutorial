1ecc77ffe395ef52fe94353e82bc5d3ab7501d8b
HarinadhJava <harinadhjava311@gmail.com>
2024-06-07 09:40:15 +0530

    if while loop conditions

---
+++ src/com/java/fundamentals/IfWhileLoop.java
@@ -0,0 +1,33 @@
+package com.java.fundamentals;
+
+public class IfWhileLoop {
+
+    public static void main(String[] args) {
+        IfWhileLoop.m1();
+    }
+
+    static void m1() {
+
+        //for loop need to repeat 5 times
+
+        for (int i = 0; i < 100; i++) {
+
+            System.out.println("Hello: " + i);
+
+            for (int j = 1; j < 6; j++) {
+
+                System.out.println("Nested for loop");
+            }
+        }
+
+        //while loop
+
+        int no = 1;
+
+        while (no < 100) {
+            System.out.println("while loop :" + no);
+            no++;
+        }
+
+    }
+}
